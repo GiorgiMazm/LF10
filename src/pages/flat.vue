b707acb308d63abae4a82463a81f1fb93d682169
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2023-09-06 11:23:18 +0200

    created home pae and add flat page

---
+++ src/pages/flat.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Flat formular</div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,27 @@
-<script setup lang="ts">
-const message = "This is our cool app!";
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div class="text-5xl text-red-600">
-    {{ message }}
+  <div>
+    <section>
+      <div class="container mx-auto">
+        <div class="text-center pt-20">
+          <h1 class="text-5xl">Welcome to our page!</h1>
+          <h2 class="text-3xl pt-12 pb-4">Select your form</h2>
+          <ul class="flex flex-col items-center">
+            <li
+              class="bg-lime-400 w-[15%] px-6 rounded-xl hover:bg-blue-400 py-4 my-4"
+            >
+              <NuxtLink href="/flat">Wohngeldantrag</NuxtLink>
+            </li>
+
+            <li
+              class="w-[15%] bg-lime-400 px-6 rounded-xl hover:bg-blue-400 py-4"
+            >
+              <NuxtLink href="/flat">Wohngeldantrag</NuxtLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
