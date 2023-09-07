2d3528f8ea227128b6a2de7d510cca79db47cac4
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2023-09-07 12:19:57 +0200

    save object dynamic in DB

--- src/components/AppForm.vue
+++ src/components/AppForm.vue
@@ -1,16 +1,31 @@
 <script setup lang="ts">
 const { addForm } = useForm();
 async function createForm() {
-  await addForm({ name: "Giorgi" });
+  await addForm(dataObject.value);
+  console.log(dataObject.value);
 }
+
+const formFields = ["First", "Lastname", "Age", "Nationality", "sex"];
+
+function createObject(data: object) {
+  //@ts-ignore
+  dataObject.value[data.name] = data.value;
+}
+
+const dataObject = ref({});
 </script>
 
 <template>
   <div>
     <form>
-      <text-input label="Name" placeholder="John" />
-      <text-input label="Name" placeholder="John" />
-      <button @click.prevent="createForm">Submit</button>
+      <text-input
+        v-for="filed in formFields"
+        :label="filed"
+        :placeholder="filed"
+        :field-name="filed"
+        @passInputValue="createObject"
+      />
+      <button class="my-2" @click.prevent="createForm">Submit</button>
       <br />
       <NuxtLink href="/">Zurück</NuxtLink>
     </form>
--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -2,14 +2,25 @@
 interface propsInterface {
   label: string;
   placeholder: string;
+  fieldName: string;
 }
 const props = defineProps<propsInterface>();
+
+const name = ref("");
 </script>
 
 <template>
   <div class="mt-3">
     <label>{{ props.label }}</label>
     <input
+      v-model="name"
+      @keyup="
+        $emit(
+          'passInputValue',
+          { name: props.fieldName, value: name },
+          props.fieldName
+        )
+      "
       class="ml-2 w-[10%] p-1 inline-block border border-gray-400 rounded"
       type="text"
       :placeholder="props.placeholder"
