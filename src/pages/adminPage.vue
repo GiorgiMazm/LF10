d729eb5b7c4f2bc139fa213bd7e62bf106e376fa
o00161 <owen.harperjones@tchibo.de>
2023-09-12 10:10:00 +0200

    made adjustments to admin page

--- src/pages/adminPage.vue
+++ src/pages/adminPage.vue
@@ -1,10 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { getForm } = useForm();
+const formArray = await getForm();
+</script>
 
 <template>
   <div>
     <section>
       <div class="container mx-auto">
         <h1 class="text-center text-5xl mt-8">Administrator Page</h1>
+      </div>
+      <div v-for="(value, key) in formArray">
+        {{ value }}
       </div>
     </section>
   </div>
