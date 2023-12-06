38389b4debcf18282feab0810469907b2119a075
o00161 <owen.harperjones@tchibo.de>
2023-12-06 16:27:08 +0100

    refactor

--- server/api/requests/get.ts
+++ server/api/requests/get.ts
@@ -1,7 +1,7 @@
-import connectDb from "../../plugins/connectDb";
+import { connector } from "../../plugins/connector/connectorDB";
 
 export const getRequest = async () => {
-  const dbConnection = await connectDb();
+  const dbConnection = await connector();
   if (dbConnection) {
     const forms = dbConnection.collection("forms");
     const unacceptedForms = forms.find({ _isApproved: null });
